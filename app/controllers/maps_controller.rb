8de439f39d8aab36117b9cfb9656f46cf79afb52
Peter Tinti & chris lind <student@epicodus.com>
2014-04-24 16:35:20 -0700

    add the merged files from first pull request/work by maigen and anne

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,7 @@
 class MapsController < ApplicationController
   def index
+    @map = Map.new
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "maps#index"
+   resources :maps
+   root "maps#index"
+
+
 end
