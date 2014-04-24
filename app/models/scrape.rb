89128015d2cfe9225d39fe056687da6ed654d379
Peter Tinti & chris lind <student@epicodus.com>
2014-04-24 16:52:59 -0700

    adds scraping of all child links and properly formats hashes

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -3,21 +3,61 @@ class Scrape < ActiveRecord::Base
   validates :name, presence: true
 
   def web_scrape(url)
-    begin
-      doc = Nokogiri::HTML(open(url))
-      text  = doc.at('body').inner_text
-      locationsText = text.scan(/.*is filming at.*/)
-      movie_locations = {}
+      begin
+        doc = Nokogiri::HTML(open(url))
 
+        text  = doc.at('body').inner_text
+        locationsText = text.scan(/.*is filming at.*/)
+        movie_locations = {}
 
-      locationsText.each do |lt|
-        movie = lt.split(" is filming at ")[0]
-        location = lt.split(" is filming at ")[1].scan(/^[^\(]*/)
-        movie_locations[movie] = location.join.rstrip
+        locationsText.each do |lt|
+          begin
+          movie = lt.split(" is filming at ")[0]
+          movie.tr!('•', '')
+          location = lt.split(" is filming at ")[1].scan(/^[^\(]*/)
+          movie_locations[movie] = location.join.rstrip
+          rescue
+            puts "some error"
+          end
+        end
+        movie_locations
+      rescue
+        puts "error"
       end
-      movie_locations
-    rescue
-      return false
     end
-  end
+
+    def links_one_deep(url)
+      doc = Nokogiri::HTML(open(url))
+      links = []
+      raw_links = doc.search("a").to_a
+
+      raw_links.each do |raw_link|
+        lnk = raw_link.to_s.match(/http.*\/"/).to_s
+        link = lnk[0..lnk.length-2]
+        if link.length == 43
+          links << link
+        end
+      end
+      links
+    end
+
+    def scrape_one_deep(url)
+      movies = {}
+      links = links_one_deep(url)
+      links.each do |link|
+        if link != nil
+          begin
+            linked_movies = web_scrape(link)
+            begin
+              movies.merge!(linked_movies)
+            rescue
+              puts "couldnt merge"
+            end
+          rescue
+            puts "couldnt scrape #{link}"
+          end
+        end
+      end
+      movies
+    end
 end
