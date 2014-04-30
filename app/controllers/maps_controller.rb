3a3f747ec8df12a834a05ee1306639e3b062e138
maigen skjelstad & Chris Lind <student@epicodus.com>
2014-04-30 16:40:55 -0700

    add the files to show maps properly

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,15 @@
 class MapsController < ApplicationController
+  def show
+    @map = Map.find(params[:id])
+    @locations = Location.all[31..40]
+    @nearby_locations = Location.near(@map.location)
+    @nearby_hash = Gmaps4rails.build_markers(@nearby_locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+      marker.infowindow location.title
+    end
+  end
+
   def index
     @map = Map.new
     @locations = Location.all[31..40]
@@ -11,18 +22,31 @@ class MapsController < ApplicationController
   end
 
   def create
-    # Gmaps4rails.clear_markers
-    @map = Map.new(map_params)
-    if @map.save
-      respond_to do |format|
-        format.html { redirect_to :back }
-        format.js
-      end
-    else
-      flash[:alert] = 'Bad Search'
-      redirect_to :root
+    @nearby_locations = Location.near(map_params[:location])
+    @nearby_hash = Gmaps4rails.build_markers(@nearby_locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+      marker.infowindow location.title
     end
 
+    @map = Map.new(map_params)
+    if @map.save
+      redirect_to map_path(@map)
+    else
+      flash[:alert] = 'Bad Search'
+      redirect_to :back
+    end
+
+  end
+
+  def update
+    @map = Map.find(params[:id])
+    if @map.update(map_params)
+      redirect_to map_path(@map)
+    else
+      flash[:alert] = 'Bad Search'
+      redirect_to :back
+    end
   end
 
 
